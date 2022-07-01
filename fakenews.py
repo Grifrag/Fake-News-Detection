32dea2b4ad5778beae43fcc9ba5f389561a2f4d9
Grifrag <66517447+Grifrag@users.noreply.github.com>
2022-07-01 04:45:27 +0300

    Delete fakenews.py

--- fakenews.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Embedding,LSTM,Dense,Dropout
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.preprocessing.text import one_hot
